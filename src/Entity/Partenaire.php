340e71fc277aad5a95d49960b023aeaf91859829
moussa70 <Papemoussaseck70@gmail.com>
2020-01-25 15:05:33 +0000

    ajouter partenaire

---
+++ src/Entity/Partenaire.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\PartenaireRepository")
+ */
+class Partenaire
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $ninea;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $registreCommerce;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNinea(): ?string
+    {
+        return $this->ninea;
+    }
+
+    public function setNinea(string $ninea): self
+    {
+        $this->ninea = $ninea;
+
+        return $this;
+    }
+
+    public function getRegistreCommerce(): ?string
+    {
+        return $this->registreCommerce;
+    }
+
+    public function setRegistreCommerce(string $registreCommerce): self
+    {
+        $this->registreCommerce = $registreCommerce;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
